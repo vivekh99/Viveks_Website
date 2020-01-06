58eaa16b32d73cc0b7ac622a2c4b2512e80f4c1e
Vivek Hanasoge <46605922+vivekh99@users.noreply.github.com>
2020-01-06 17:23:56 -0500

    added a feedback form - not tested

---
+++ feedback_form.php
@@ -0,0 +1,24 @@
+<?php
+	if ( isset( $_POST['submitBtn'] ) ) { 
+
+		$mail_to = 'vinvek@outlook.com';
+		$subject = 'Test';
+		$telephone = $_POST['phone_number'];
+		$email = $_POST['email'];
+		$name = $_POST['name_info'];
+		$job_description = $_POST['description'];	
+
+		$message = $telephone . " ";
+		$message .= $email . " ";
+		$message .= $name . " ";
+		$message .= $job_description;
+
+
+		if(mail($email, $subject, $message)){
+			echo "mail sent!";
+		}
+		else{
+			echo "FAILURE";
+		}
+	}
+?>
